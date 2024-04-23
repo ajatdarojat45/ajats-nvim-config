83795c230ad3c8eab02f4ec646e01bab48488741
Ajat Darojat <ajatdarojat45@gmail.com>
2024-04-23 22:29:53 +0700

    migrate ibl to v3

--- lua/indentblankline.lua
+++ lua/indentblankline.lua
@@ -1,18 +1,34 @@
-vim.opt.list = true
---vim.opt.listchars:append "space:⋅"
---vim.opt.listchars:append "eol:↴"
-vim.opt.termguicolors = true
+-- indent blankline V2 setup
+--vim.opt.list = true
+--vim.opt.termguicolors = true
 
-require("indent_blankline").setup {
-  space_char_blankline = " ",
+--require("indent-blankline").setup {
+--space_char_blankline = " ",
   -- char = "|",
-  show_current_context_start = true,
-  show_current_context = true,
-  context_patterns = {
-    "class", "return", "function", "method", "^if", "^while", "jsx_element", "^for", "^object",
-    "^table", "block", "arguments", "if_statement", "else_clause", "jsx_element",
-    "jsx_self_closing_element", "try_statement", "catch_clause", "import_statement",
-    "operation_type"
-  },
-}
+ -- show_current_context_start = true,
+--  show_current_context = true,
+--  context_patterns = {
+--    "class", "return", "function", "method", "^if", "^while", "jsx_element", "^for", "^object",
+--    "^table", "block", "arguments", "if_statement", "else_clause", "jsx_element",
+--    "jsx_self_closing_element", "try_statement", "catch_clause", "import_statement",
+--    "operation_type"
+--  },
+--}
+
+
+-- indent blankline V3 setup
+local scope = "focus"
+local indent = "passive"
+
+local hooks = require("ibl.hooks")
+
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "focus", { fg = "#7486bd" })
+	vim.api.nvim_set_hl(0, "passive", { fg = "#41425e" })
+end)
+
+require("ibl").setup({
+	scope = { highlight = focus },
+	indent = { highlight = indent }
+})
 
