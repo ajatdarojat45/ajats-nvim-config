520d219e59a6b5b23c0de214a16bd5978bb07f53
Ajat Darojat <ajatdarojat45@gmail.com>
2024-10-26 20:24:02 +0700

    update bufferline

--- lua/buffer_line.lua
+++ lua/buffer_line.lua
@@ -1,19 +1,23 @@
 vim.opt.termguicolors = true
 require("bufferline").setup{
-    options = {
-      indicator = {
-        icon = '▎',
-        style = 'underline',
-      },
-      hover= {
-        enabled = true,
-        delay = 200,
-        reveal = {'close'}
-      },
-      diagnostics = 'coc',
-      diagnostics_indicator = function(count, level)
-        local icon = level:match("error") and " " or " "
-        return " " .. icon .. count
-      end
+  options = {
+    mode = "buffers",
+    indicator = {
+      icon = '▎', 
+      style = 'underline',
     },
+    close_icon = '✗',
+    buffer_close_icon = '✗',
+    diagnostics = "nvim_lsp",
+    diagnostics_indicator = function(count, level)
+      local icon = level:match("error") and " " or " "
+      return " " .. icon .. count
+    end
+
+  },
+  highlights = {
+    buffer_selected = {
+      fg = '#73aaf9',
+    },
+  }
 }
