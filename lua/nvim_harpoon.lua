302a5ac6201c0761e9a562032d0f61b96a4e22d4
Ajat Darojat <ajatdarojat45@gmail.com>
2024-10-29 12:57:27 +0700

    install harpoon

--- lua/nvim_harpoon.lua
+++ lua/nvim_harpoon.lua
@@ -1,31 +1,62 @@
 local harpoon = require("harpoon")
 
 -- REQUIRED
-harpoon:setup({})
+harpoon:setup()
+-- REQUIRED
 
 -- basic telescope configuration
-local conf = require("telescope.config").values
+--local conf = require("telescope.config").values
+--local function toggle_telescope(harpoon_files)
+--    local file_paths = {}
+--    for _, item in ipairs(harpoon_files.items) do
+--        table.insert(file_paths, item.value)
+--    end
+--
+--    require("telescope.pickers").new({}, {
+--        prompt_title = "Harpoon",
+--        finder = require("telescope.finders").new_table({
+--            results = file_paths,
+--        }),
+--        previewer = conf.file_previewer({}),
+--        sorter = conf.generic_sorter({}),
+--    }):find()
+--end
+
 local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
+    local finder = function()
+        local paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+            table.insert(paths, item.value)
+        end
+
+        return require("telescope.finders").new_table({
+            results = paths,
+        })
     end
 
     require("telescope.pickers").new({}, {
         prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
+        finder = finder(),
+        previewer = false,
+        sorter = require("telescope.config").values.generic_sorter({}),
+        attach_mappings = function(prompt_bufnr, map)
+            map("i", "<C-d>", function()
+                local state = require("telescope.actions.state")
+                local selected_entry = state.get_selected_entry()
+                local current_picker = state.get_current_picker(prompt_bufnr)
+
+                table.remove(harpoon_files.items, selected_entry.index)
+                current_picker:refresh(finder())
+            end)
+            return true
+        end,
     }):find()
 end
 
+vim.keymap.set("n", "<C-p>", function() harpoon:list():add() end)
+--vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
     { desc = "Open harpoon window" })
-
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
---vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
